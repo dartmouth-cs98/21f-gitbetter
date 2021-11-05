da08b5f57a6b5c4ff608dc15f3878e512e2f232d
Irene Lam <irene.x.lam.22@dartmouth.edu>
2021-11-04 23:26:26 -0400

    Fixed navigation, all pages display properly

--- react/src/App.js
+++ react/src/App.js
@@ -1,21 +1,37 @@
-import Navigation from './Navigation.js'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
 import Home from './Home';
 import Help from './Help';
+import Navigation from './Navigation.js'
 import './App.css';
 
 function App() {
   return (
-    <div id="app" className="wrapper">
-      <Router>
-        <Navigation />
-        <Routes>
-          <Route exact path="/" component={Home} />
-          <Route path="/help" component={Help} />
-        </Routes>
-      </Router>
-      <div className="divider"></div>
-      <router-view />
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="help" element={<Help />} />
+        <Route path="users/*" element={<Users />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Users() {
+  return (
+    <div>
+      <nav>
+        <Link to="me">My Profile</Link>
+      </nav>
+
+      <Outlet />
     </div>
   );
 }
