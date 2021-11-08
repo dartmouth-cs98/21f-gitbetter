11d7c10310076ddeee15991bfc87cff52e7501c6
Kimberley R <kimberley.rangel.campos.22@dartmouth.edu>
2021-11-08 16:23:18 -0500

    Made method to display folders and file in directory

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,12 +3,46 @@
         <div> 
           <b>Project Structure:</b>
         </div>
+        <button @click="readDirectory">Press Here</button>
+    <v-btn  v-for="file in files" :key="file.id">
+    {{file}}
+ </v-btn>
+
+  
+         
     </div>
 </template>
 
 <script>
+const fs = require('fs-extra')
 export default {
   name: 'Sidebar',
+  data () {
+    return {
+      dir:'C:/Users/GWC-NYC-13/21f-gitbetter',
+      files:[],
+      file:"",
+
+    }
+  },
+  methods: {
+    readDirectory() {
+      fs.readdir(this.dir, (err, dir) => {
+      console.log(dir);
+      for(let filePath of dir)
+      console.log(filePath)
+      this.files = dir
+      })
+    },
+    displayDirectory(){
+      fs.readdir(this.dir, (err, dir) => {
+      console.log(dir);
+      for(let filePath of dir)
+      console.log(filePath)
+      this.files = dir
+      })
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
