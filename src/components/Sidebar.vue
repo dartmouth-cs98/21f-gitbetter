5ccda7687b1a48837861f2775ce692c3df264066
Kimberley R <kimberley.rangel.campos.22@dartmouth.edu>
2021-11-10 02:45:59 -0500

    Added some styling

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -3,14 +3,17 @@
         <div> 
           <b>Project Structure: {{this.dir}}</b>
         </div>
-    <v-btn  v-for="file in files" :key="file.id">
-    {{file}}
- </v-btn>   
+
+          <md-list class="list">
+            <md-list-item  class="file" v-for="file in files" :key="file.id">{{file}}</md-list-item>
+          </md-list>
+          
     </div>
 </template>
 
 <script>
-const fs = require('fs-extra')
+const fs = require('fs-extra'),
+path = require('path');
 export default {
   name: 'Sidebar',
   data () {
@@ -18,33 +21,39 @@ export default {
       dir: process.cwd(),
       files:[],
       file:"",
+      all_files:[],
 
     }
   },
   // created is the equivalent to componentDidMount
   created(){
     console.log("it was created!")
-    this.readDirectory()
+    this.readDirectory(this.dir)
   },
   methods: {
-    readDirectory() {
-      fs.readdir(this.dir, (err, dir) => {
+    readDirectory(directory) {
+      var files = fs.readdirSync(directory)
+      for (var i in files){
+        var next = path.join(directory, files[i])
+        console.log(next)
+        if (fs.lstatSync(next).isDirectory() == true){
+          if (files[i] == 'node_modules'){
+            console.log("Skipping node_module folder")
+          }
+          else{
+            this.readDirectory(next)
+          }
+        }
+      }
+      fs.readdir(directory, (err, dir) => {
       console.log(dir);
       for(let filePath of dir)
       console.log(filePath)
-      console.log(__dirname);
+      // console.log(__dirname);
       console.log(process.cwd())
       this.files = dir
       })
     },
-    displayDirectory(){
-      fs.readdir(this.dir, (err, dir) => {
-      console.log(dir);
-      for(let filePath of dir)
-      console.log(filePath)
-      this.files = dir
-      })
-    }
   }
 }
 </script>
@@ -57,4 +66,17 @@ export default {
       border-width: 1px;
       border-style: solid;
   }
+
+  .file {
+    background-color: #FAE5DF;
+    // min-height: 0px;
+  }
+  
+  .md-list-item-.md-list-item-content.md-ripple.md-disabled {
+    min-height: 0px;
+  }
+  .list{
+    padding: 0
+  }
+
 </style>
