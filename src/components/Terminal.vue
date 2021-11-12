0668453a896eb650cb7447b255811d4e1db53093
Kimberley R <kimberley.rangel.campos.22@dartmouth.edu>
2021-11-12 13:50:39 -0500

    Trying to get the window to scroll to the bottom

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -1,12 +1,13 @@
 // Adapted from: https://vuejsexamples.com/component-vuejs-to-simulate-a-terminal/
 <template>
   <div class="terminal" >
-    <v-shell  
+    <v-shell
       :banner="banner"
       :shell_input="send_to_terminal"
       :commands="commands"
       @shell_output="prompt"
     ></v-shell>
+    <div id="anchor"></div>
   </div>
 </template>
 
@@ -17,8 +18,10 @@ import replicate from '../../replicate_repo.js';
 var run_command = require('../../run_command');
 
 Vue.use(shell);
+// const anchor = document.querySelector('#input-line')
 
 export default {
+  el: '#app',
   data() {
     return {
       send_to_terminal: '',
@@ -56,6 +59,15 @@ export default {
   
   methods: {
     prompt(value) {
+      console.log("when does this function run?")
+      this.scrollToElement();
+      // window.scrollTo(0, anchor.scrollHeight);
+      // anchor.scrollTop = anchor.scrollHeight - anchor.clientHeight
+       
+  //      anchor.scrollIntoView({
+  //   behavior: 'smooth',
+  //   block: 'end'
+  // });
       try {
           //const send_value = async () => {
             run_command.run_script(value, null, (error, returnVal)=> {
@@ -63,13 +75,23 @@ export default {
                 //eventually a look up table for errors will go here
               } 
             this.send_to_terminal = returnVal;
-
-            });
+            
+            }); 
+        
       }
       catch(error) {
         this.send_to_terminal = "Command not found"
       }
     },
+    scrollToElement() {
+    const el = this.$refs.scrollToMe;
+    console.log(this.$refs)
+
+    if (el) {
+      // Use el.scrollIntoView() to instantly scroll to the element
+      el.scrollIntoView({behavior: 'smooth'});
+    }
+  }
   }
 };
 </script>
@@ -77,5 +99,7 @@ export default {
   .terminal{
     height: 100%;
     overflow: auto;
+    
   }
+
 </style>
