10924c21f90f1d6aa41bdaa6ef39fa16e15515e9
Joanna Liu <joanna.y.liu.22@dartmouth.edu>
2021-11-13 02:42:37 -0500

    demo screenshots for text visualization working

---
+++ src/components/DemoViz.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <p v-if="!this.imgPath()"> Window for the git visualizations!</p>
+      <img :src="this.imgPath()"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemoViz',
+  props: {
+    command: String  
+  },
+  methods: {
+    imgPath() {
+        switch (this.command) {
+            case "git branch":
+                return require("../assets/demo/branch.png");
+            case "git diff":
+                return require("../assets/demo/diff.png");
+            case "git status":  
+                return require("../assets/demo/status.png");  
+            case "git log":  
+                return require("../assets/demo/log.png");  
+            default:
+                return;    
+        }
+    }
+  }
+}
+</script>
