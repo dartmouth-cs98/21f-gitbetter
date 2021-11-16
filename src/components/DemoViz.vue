9e76c7c99052996d82ce7acb2a0039d511796eda
Hailey Fox <hailey.p.fox.22@dartmouth.edu>
2021-11-16 15:39:38 -0500

    Added updated visualizations to code

--- src/components/DemoViz.vue
+++ src/components/DemoViz.vue
@@ -20,10 +20,19 @@ export default {
             return require("../assets/demo/commit-dark.png");
         } else if (this.command.startsWith("git pull")) {
             return require("../assets/demo/pull-dark.png");
-        } else if (this.command)
+        } else if (this.command == "git fetch") {
+            return require("../assets/demo/fetch-dark.png")
+        } else if (this.command.startsWith("git revert")) {
+            return require("../assets/demo/revert-dark.png")
+        } else if (this.command == "git push") {
+            return require("../assets/demo/push-dark.png")
+        } else if (this.command == "git branch -u origin/master") {
+            return require("../assets/demo/origin-master-dark.png")
+        }
+        
         // } else if (this.command == "gitbetter visualize") {
         //     return require("../assets/demo/start-state-dark.png")
-        } else {
+        else {
             return;
         }
     }
