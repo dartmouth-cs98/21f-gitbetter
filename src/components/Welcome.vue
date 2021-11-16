fa6c0bd8c999670dccd1d2004c2465281e1e95e2
ediewilson <59631032+ediewilson@users.noreply.github.com>
2021-11-16 17:01:45 -0500

    Changing welcome page

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,23 +1,36 @@
 <template>
   <div class="main top-welcome">
-      <div class="page-flex">
-      <div class="top-directory">
-          <div class="start-box">
-              <h1>Start</h1>
-              <h2>+ Create new file</h2>
-              <h2>+ Open file</h2>
-              <h2>+ Clone git repository</h2>
-              <h1/>
-              <h1>Recent</h1>
-              <h2>+ CS98 GitBetter</h2>
-          </div>
-      </div>
-      <div class="vl"></div>
-     
-      <div class= "top-help" style="padding-left:45px;">
-              <nav class="panel" style="box-shadow:none;">
-                <h1 class="help-header"> Search for commands... </h1>
-                <div class="panel-block" style="border-bottom:none;">
+      <div class="page-flex">        
+            <div class="top-directory">
+                <div class="logo">
+                    <img src="../assets/logo.png" />
+                    <p class="subtitle">Git, simplified.</p>
+                    
+                    <div class="wrapper" @click="$router.push('/home')" v-on:click="gitStarted" >
+                        <div class="cta">
+                            <span>Git Started</span>                            
+                        </div>
+                    </div>
+                </div>
+                <div class="start-box">
+                    <h1 class="section-header">Start</h1>
+                    <div class="start-options">
+                    <h2>+ Create new file</h2>
+                    <h2>+ Open file</h2>
+                    <h2>+ Clone git repository</h2>
+                    </div>
+                    <h1 class="section-header">Recent</h1>
+                    <h2>+ CS98 GitBetter</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="vl"></div>
+
+        <div class= "top-help">
+            <nav class="panel">
+                <h1 class="section-header help-header"> Search for commands...</h1>
+                <div class="panel-block search-comp">
                     <p class="control has-icons-left">
                         <input class="input" type="command" v-model="searchInput" @input="searchCommands(searchInput)" placeholder="How do I do this?">
                         <span class="icon is-small is-left">
@@ -25,27 +38,19 @@
                         </span>
                     </p>
                 </div>
-                <div class="panel-block" style="font-weight:500;font-size:20px;" v-if="commandOpts.length > 1">
-                    {{ commandOpts.length }} results found...
+                <div class="panel-block results-parent">
+                    <div class="panel-block results" v-if="commandOpts.length > 1">
+                        {{ commandOpts.length }} results found...
+                    </div>
+                    <div class="panel-block results" v-if="commandOpts.length == 1">
+                        {{ commandOpts.length }} result found...
+                    </div>
+                    <div v-for="match in commandOpts" :key="match.id" class="panel-block command-blocks">
+                        {{ match.command }}: {{ match.desc }}
+                    </div>
                 </div>
-                <div class="panel-block" style="font-weight:500;font-size:20px;" v-if="commandOpts.length == 1">
-                    {{ commandOpts.length }} result found...
-                </div>
-                <div v-for="match in commandOpts" :key="match.id" class="panel-block" style="background-color:rgba(225,225,225,0.3);">
-                    {{ match.command }}: {{ match.desc }}
-                </div>
-              </nav>
-      </div>
-      
-      </div>
-      <img src="../assets/logo.png" />
-      <button 
-        @click="$router.push('/home')"
-        v-on:click="gitStarted"
-        class="center-button button"
-        >
-           Git Started
-      </button>
+            </nav>
+        </div>
       <loading />
   </div>
 </template>
@@ -89,7 +94,7 @@ export default {
       },
       searchCommands (input) {
           let matches = [];
-          if(input.length > 2) {
+          if(input.length > 2 && input !=="git") {
             for (var key in gitCommands) {
                 if (key.includes(input) || gitCommands[key].includes(input)) {           
                     console.log(key, gitCommands[key]);
@@ -123,22 +128,24 @@ h1 {
 h2 {
     font-size: 20px;
 }
-input[type="text"] {
-    padding: 10px;
-    width: 80%;
-    border-radius: 20px;
-    font-size: 18px;
-    font-style: italic;
-    border-color: #1A61EB;
-    padding-bottom: 10px;
-    margin-bottom: 10px;
-}
+
 img {
-    position: absolute;
-    top: 40%;
-    left: 30%;
-    width: 40%;
+    height: auto;
+    width: 100%
 }
+.logo {
+    padding: 30px 30px 0 10px;
+}
+
+.subtitle {
+    padding-left: 50%;
+    position: relative;
+    top: -15%;
+    font-style: italic;
+    color: rgb(39, 39, 39);
+    font-size: 175%;
+}
+
 .columns {
     height: 100%;
 }
@@ -146,33 +153,36 @@ img {
     background: linear-gradient(to right, #DA91E5, #9953F1 50%, #53336B);
 }
 .page-flex {
-    display: flex;
     width: 100%;
     height: 100%;
 }
 .top-directory {
     width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 }
 .top-help {
     width: 50%;
-}
-.center-button {
+    height: 100%;
     position: absolute;
-    top: 65%;
-    left: 44%;
-    padding: 10px 15px;
-    background-color: white;
-    border-color: #4200FF;
-    font-weight: 650;
-    cursor: pointer;
+    top: 0;
+    right: 0;
+    padding-left: 45px;
 }
-.center-button:hover {
-    color: rgb(0, 0, 0);
-    box-shadow: 0 10px 20px rgba(209, 161, 244, 0.4);
+
+.start-options {
+    display: flex;
+    flex-direction: column;
 }
+
 .start-box {
     color: white;
     padding-left: 45px;
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 45px;
 }
 .vl {
     border-left: 1.5px solid black;
@@ -183,15 +193,107 @@ img {
 }
 .panel {
     color: white;
+    box-shadow: none;
+    width: 90%
 }
 .panel-block {
     color: white;
     border: none;
     border-radius: 0 !important;
 }
+
+.command-blocks {
+    background-color:rgba(225,225,225,0.3);
+    width: 100%;
+}
+
 .help-header {
+    padding-top: 45px;
     padding-left: 10px;
 }
+
+.section-header {
+    padding-bottom:0px;
+}
+
+.search-comp {
+    border-bottom: none !important;
+}
+
+.results {
+    font-weight:500;
+    font-size:20px;
+}
+
+.results-parent {
+    padding-bottom: 45px;
+    max-height: 75vh;
+    overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+}
+
+div::-webkit-scrollbar {
+  display: none; /* for Chrome, Safari, and Opera */
+}
+
+/* BUTTPN CSS */
+@import url('https://fonts.googleapis.com/css?family=Poppins:900i');
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.wrapper {
+  display: flex;
+  justify-content: center;
+  position: relative;
+  top: -25px;
+}
+
+.cta {
+    display: flex;
+    padding: 10px 30px;
+    text-decoration: none;
+    font-family: 'Poppins', sans-serif;
+    font-size: 25px;
+    color: white;
+    background: #272727;
+    transition: 1s;
+    box-shadow: -6px 6px 0 black;
+    transform: skewX(-15deg);
+}
+
+.cta:focus {
+   outline: none; 
+}
+
+.cta:hover {
+    transition: 0.5s;
+    box-shadow: -10px 10px 0 #583575;
+}
+
+
+@keyframes color_anim {
+    0% {
+        fill: white;
+    }
+    50% {
+        fill: #583575;
+    }
+    100% {
+        fill: white;
+    }
+}
+
+/* END BUTTON CSS */
 
 @media only screen and (max-width: 970px) {
     .center-button {
