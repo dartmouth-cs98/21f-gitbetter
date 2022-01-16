57c17f461f2b51a040b8846cc225b393bf77ee17
Joanna Liu <joanna.y.liu.22@dartmouth.edu>
2022-01-16 02:11:52 -0500

    wip: default viz w/ hard coded tree

---
+++ src/components/DefaultViz.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <vo-basic :data="chartData" v-on:click="handleClick"></vo-basic>
+  </div>
+</template>
+ 
+<script>
+import { VoBasic } from "vue-orgchart";
+import "vue-orgchart/dist/style.min.css";
+
+export default {
+  name: 'DirectoryTree',
+  components: { VoBasic },
+  created() {
+    this.chartData = {
+      name: "GitBetterTestRepository",
+      children: [
+        { name: "Merging.py" },
+        { name: "README.md" },
+        { name: "Searching.py" },
+        { name: "Sorting.py" },
+      ]
+    };
+  },
+  methods: {
+    handleClick: e => {
+      alert(e);
+    }
+  }
+};
+</script>
---
+++ src/components/Visualization.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="justify-content:center;">
+      <!-- 
+          This will become the default so: <DirectoryTree v-else/>
+          and other components will be: <SomeViz v-if="this.command==='git command'">
+      -->
+      <DirectoryTree/>
+  </div>
+</template>
+
+<script>
+import DirectoryTree from './DefaultViz.vue'
+
+export default {
+  name: 'Viz',
+  props: {
+    command: String  
+  },
+  components:{
+      DirectoryTree
+  }
+}
+</script>
