6bb9b726a8347ca2bcbc88c95348346135b560a2
Joanna Liu <joanna.y.liu.22@dartmouth.edu>
2022-01-19 19:06:12 -0500

    first layer of files/directories

--- src/components/DefaultViz.vue
+++ src/components/DefaultViz.vue
@@ -7,24 +7,51 @@
 <script>
 import { VoBasic } from "vue-orgchart";
 import "vue-orgchart/dist/style.min.css";
+const path = require('path');
+const fs = require('fs-extra');
 
 export default {
   name: 'DirectoryTree',
   components: { VoBasic },
+  data () {
+    return {
+      dir: process.cwd(),
+      files: [],
+      directories: [],
+      file: "",
+    }
+  },
   created() {
     this.chartData = {
-      name: "GitBetterTestRepository",
-      children: [
-        { name: "Merging.py" },
-        { name: "README.md" },
-        { name: "Searching.py" },
-        { name: "Sorting.py" },
-      ]
+      name: path.basename(process.cwd()).replace('.GB', ''),
+      children: []
     };
+    this.dirsOnly(this.dir)
+    this.filesOnly(this.dir)
+    this.directories.map(item => this.chartData.children.push({name: item, children: []}))
+    this.files.map(item => this.chartData.children.push({name: item, children: []}))
+
   },
   methods: {
     handleClick: e => {
       alert(e);
+    },
+    filesOnly(root) {
+      let fileNames = fs.readdirSync(root, {withFileTypes: true})
+      .filter(item => !item.isDirectory())
+      .map(item => item.name)
+      
+      console.log("File names", fileNames)
+      this.files = fileNames;
+
+    },
+    dirsOnly(root) {
+      let dirNames = fs.readdirSync(root, {withFileTypes: true})
+      .filter(item => item.isDirectory())
+      .map(item => item.name)
+      
+      console.log("Dir names", dirNames)
+      this.directories = dirNames;
     }
   }
 };
