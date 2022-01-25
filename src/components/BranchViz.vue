a5e5194483442668069dbf8c2d97248897335801
Irene Lam <irene.x.lam.22@dartmouth.edu>
2022-01-24 22:38:01 -0500

    branch changed to buttons, testing interactive button press

--- src/components/BranchViz.vue
+++ src/components/BranchViz.vue
@@ -1,18 +1,16 @@
 <template>
 <div>
-    <!-- <div v-for="direc in branches" :key="direc" >
-        - {{ direc }}
-    </div> -->
-    <!-- {{this.branchData}} -->
-    <button :click="this.onButtonClick">some ndsfdjsk</button>
-    <vo-basic :key="this.branchData.name" :data="this.branchData" direction="l2r"></vo-basic>
+    <div v-for="direc in branches" :key="direc" >
+        <button v-on:click="switchBranch(direc)"> {{ direc }} </button>
+    </div>
+    <!-- <vo-basic :key="this.branchData.name" :data="this.branchData" direction="l2r"></vo-basic> -->
     
 </div>
 </template>
 
 <script>
-import { VoBasic } from "vue-orgchart";
-import "vue-orgchart/dist/style.min.css";
+// import { VoBasic } from "vue-orgchart";
+// import "vue-orgchart/dist/style.min.css";
 
 const pty = require("node-pty");
 const ipc = require("electron").ipcRenderer
@@ -20,62 +18,46 @@ const ipc = require("electron").ipcRenderer
 export default {
     name: 'BranchViz',
     components: {
-        VoBasic,
+        // VoBasic,
     },
     data () {
         return {
             branches: ['firstName', 'somewhat_long_name_that_hopefully_exceeds_window_len', 'branch3'],
+            ptyProcess: pty.spawn('bash', [], {
+                name: "xterm-color",
+                cols: 80,
+                rows: 100,
+                cwd: process.CWD,
+                env: process.env
+            })
         }
     },
     methods: {
-        createNode(name) {
-            return { name }
-        },
-        onButtonClick() {
-            console.log('force render')
-            this.$set(this.branchData, 'children', this.branches.map(this.createNode))
-        }
-    },
-    created () {
-        this.branchData = {
-            name: ' ',
-            children: this.branches.map(branch => this.createNode(branch)),
+        switchBranch(branch) {
+            this.ptyProcess.write(`git checkout ${branch}\n`);
         }
     },
     mounted() {
-        var ptyProcess = pty.spawn('bash', [], {
-            name: "xterm-color",
-            cols: 80,
-            rows: 100,
-            cwd: process.CWD,
-            env: process.env
-        });
-
-        ptyProcess.on("data", (data) => {
-            if (data.startsWith('bash')) return;
+        this.ptyProcess.on("data", (data) => {
+            if (data.startsWith('bash') || data.startsWith('git')) return;
             this.branches = data.replaceAll('[m', '')
                 .split('\n').slice(0, -1)
                 .map(branch => branch.trim())
                 .map(branch => {
                     if (branch.includes('=')) return branch.split('=')[1].trim();
-                    if (branch.startsWith('*')) return branch.substring(1);
+                    // if (branch.startsWith('*')) return branch.substring(1);
                     return branch;
                 })
             console.log("branch changed");
-            this.$set(this.branchData, 'children', this.branches.map(this.createNode))
-            this.$set(this.branchData, 'name', this.branches[0])
-            window.irene = this.branchData;
-            this.$nextTick(function () {
-                console.log('branch diverged')
-            })
         });
-        ptyProcess.write('git branch --no-color\n');
+        this.ptyProcess.write('git branch --no-color\n');
         ipc.on("terminal.toTerm", function(event, data) {
+             console.log("world changes");
             if (
                 data.includes('git branch')
                 || data.includes('git switch')
                 || data.includes('git checkout')
-            ) ptyProcess.write('git branch --no-color\n');
+            ) this.ptyProcess.write('git branch --no-color\n');
         })
     },
 
