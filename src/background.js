bcd36e35e54192bdfcf5a69b096f99b554ddf87b
ZoeWortzman <zoemwortzman@gmail.com>
2022-01-30 20:59:51 -0500

    comments

--- src/background.js
+++ src/background.js
@@ -1,15 +1,14 @@
 import { app, protocol, BrowserWindow, ipcMain, dialog } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
-//import { DialogContent } from '@material-ui/core';
 
 
 const os = require("os");
 const pty = require("node-pty");
 
-var clear = require('../start_over');
+var clear = require('./utils/start_over');
 var shell = os.platform() === "win32" ? "powershell.exe" : "bash";
-var replicate = require('../replicate_repo')
+var { replicate } = require('./replicate_repo')
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -50,24 +49,20 @@ async function createWindow() {
   });
 
   ipcMain.on("terminal.toTerm", function(event, data) {
+    win.webContents.send("user_input", data);
     ptyProcess.write(data);
   });
 
   ipcMain.on("openFinder", function() {
-  dialog.showOpenDialog({
-    defaultPath:app.getPath('home'), 
-    properties:['openFile', 'openDirectory']
-    }).then((result)=> {
-      let pwd = result.filePaths[0]
-      ptyProcess.write('cd ' + pwd);
-      ptyProcess.write('\n');
-      ptyProcess.write('clear');
-      ptyProcess.write('\n');
-      replicate.replicate_repo(pwd)
-
-  }).catch((e) =>
-  console.error(e));
-})
+    dialog.showOpenDialog({
+      defaultPath:app.getPath('home'), 
+      properties:['openFile', 'openDirectory'],
+    }).then(({ filePaths })=> {
+      const [pwd] = filePaths;
+      ptyProcess.write(`cd "${pwd}" \n`);
+      replicate(pwd);
+    }).catch(console.error);
+  })
 
   // ipcMain.on("gitStarted.to")
   if (process.env.WEBPACK_DEV_SERVER_URL) {
