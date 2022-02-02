0370fc38e07cf93bc81846d15d399d3416261255
ZoeWortzman <zoemwortzman@gmail.com>
2022-02-01 21:12:05 -0500

    working on parsing git status

--- src/utils/parse_status.js
+++ src/utils/parse_status.js
@@ -1,8 +1,15 @@
-var parse_status =  async function parse_status(input) {
+async function parse_status(input) {
+    // for (let line=0; line<4, line++;) {
 
-    console.log(input)
+
+    // }
+   if (input.length > 1){
+       console.log(input)
+
+   }
+    
+
 }
-
 
 const _parse_status = parse_status;
 export { _parse_status as parse_status};
