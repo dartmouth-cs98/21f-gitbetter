f606ab4eccfdf4573e9e8b9e6d20e7effe5eb71e
Kimberley R <kimberley.rangel.campos.22@dartmouth.edu>
2022-02-06 16:59:54 -0500

    More styling needed

--- src/components/Visualizations/MergeCon.vue
+++ src/components/Visualizations/MergeCon.vue
@@ -1,11 +1,11 @@
 <template>
-    <div >
-        Merge Interface
-        {{this.mergeSteps.title}}
-        <div> {{this.mergeSteps.steps[index].step}}</div>
-        <button v-if="this.index > 0" @click="backStep">Back</button>
-        <button v-if="this.index + 1< this.content.length" @click="nextStep">Next</button>
-
+    <div class="top-layer">
+        <div class="title-area">{{this.mergeSteps.title}}</div>
+        <div class="content"> {{this.mergeSteps.steps[index].step}}</div>
+        <div id="button-area">
+            <button v-if="this.index > 0" @click="backStep"  class="button">Back</button>
+            <button v-if="this.index + 1< this.content.length" @click="nextStep"  class="button">Next</button>
+        </div>  
     </div>
 
 </template>
@@ -18,25 +18,44 @@ export default {
         return {
             mergeSteps: mergeInterface,
             content: mergeInterface.steps,
-            currStep: mergeInterface.steps[0],
             index: 0,
             }
-            },
+    },
     methods: {
         nextStep() {
-            console.log("do we reach this point?");
             this.index = this.index + 1
-            console.log(this.index)
-            console.log(this.content.length)
         },
         backStep() {
-            console.log("we are going back");
             this.index = this.index - 1
-        }
+        },
     }
 }
-/* Okay so the way one can do it is by changing the value of the {{this.content}}, we will also have buttons
-button back and button next*/
+
 </script>
+<style scoped>
+.top-layer{
+    height:100%;
+}
+.title-area{
+    font-size: 2rem;
+    color: white;
+}
+.content{
+    height:80%;
+}
+
+.button-area{
+    width:100%;
+    /* display:flex;
+    flex-direction: row;
+    align-content:space-between;
+    justify-content: space-between; */
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center
+}
+
+</style>
 
 
