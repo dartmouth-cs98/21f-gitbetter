d152b108937016b681378710c358e5fd50c18e2f
ediewilson <59631032+ediewilson@users.noreply.github.com>
2022-02-15 10:36:07 -0500

    Merging main

--- src/components/Terminal.vue
+++ src/components/Terminal.vue
@@ -11,12 +11,35 @@ const ipc = require("electron").ipcRenderer
 export default {
   data() {
     return {
-      send_to_terminal: '',
       process: Object,
       fitObj: Object,
+      branchN: "",
+      curr: "",
     };
   },
   mounted () {
+   ipc.on('branchUpdate', (event, data) => {
+      this.$store.commit('setBranchName', {name: data});
+    });
+
+    ipc.on('statUpdate', (event, data) => {
+      this.$store.commit('setStatus', {status: data});
+    });
+
+    ipc.on('setCommand', (event, data) => {
+      console.log("Data is", data)
+      if(event.key === 'Enter') {
+        this.$store.commit('setCurrCommand', {command: this.curr});
+        console.log("CURR COMM NEW COMMAND", this.curr)
+      }
+      else if (data == 'del') {
+        this.curr = this.curr.slice(0, -1);
+      }
+      else {
+        this.curr += data;
+      }
+    });
+
     this.makeScript();
     this.makeTerm();
   },
@@ -26,7 +49,6 @@ export default {
     this.$parent.$on('closeVisualization', this.resizeTerm);
 
   },
-  
   methods: {
     makeScript() {
       let recaptchaScript = document.createElement('script')
@@ -41,21 +63,41 @@ export default {
       const fitAddon = new FitAddon();
       this.fitObj = fitAddon;
       term.loadAddon(fitAddon);
+      let termParent = document.getElementById('terminal');
+      if(!termParent.firstElementChild) {
 
-      term.open(document.getElementById('terminal'));
-      // fitAddon.fit();
-      ipc.send("terminal.toTerm", "touch ~/.custom_bash_commands.sh\n")
-      //ipc.send("terminal.toTerm", "cp  gitbetter-commands.sh ~/.custom_bash_commands.sh\n")
-      ipc.send("terminal.toTerm", "source ~/.custom_bash_commands.sh\n")
-      ipc.send("terminal.toTerm", "clear\n")
+        term.open(document.getElementById('terminal'));
+        // fitAddon.fit();
+        ipc.send("terminal.toTerm", "touch ~/.custom_bash_commands.sh\n")
+        //ipc.send("terminal.toTerm", "cp  gitbetter-commands.sh ~/.custom_bash_commands.sh\n")
+        ipc.send("terminal.toTerm", "source ~/.custom_bash_commands.sh\n")
+        ipc.send("terminal.toTerm", "clear\n")
+        
 
-
-      term.onData((data) => {
-        ipc.send("terminal.toTerm", data);
-      });
-      ipc.on("terminal.incData", function(event, data) {
-        term.write(data);
-      })
+        term.onData((data) => {
+          ipc.send("terminal.toTerm", data);
+        });
+        ipc.on("terminal.incData", function(event, data) {
+          term.write(data);
+          console.log("data is", data)
+          if(data.includes("[K")) {
+            console.log('Deleeeeete')
+            ipc.send('setCommand', 'del');
+          }
+          else if(data && data.length == 1) {
+            ipc.send('setCommand', data);
+          }
+          else if(data.includes("On branch")) {
+            let branch = data.split(" ");
+            const b = branch[2].split("\r")
+            ipc.send('bnUpdate', b[0])
+          }
+          else if (data.includes("Changes to be committed") || data.includes("working tree clean")){
+            let status = data.split("\r");
+            ipc.send('statusUpdate', status)
+          }
+        })
+      }
     },
     resizeTerm() {
       try {
--- src/components/Visualizations/VizWindow.vue
+++ src/components/Visualizations/VizWindow.vue
@@ -1,36 +1,17 @@
 <template>
   <div class="vis-box">
-    <div :key="this.currCommand" class="subtitle">
-      <Viz :key="this.currCommand" :command="this.command"/> 
-    </div>
-    <!-- <div class="print-container">
-      <button @click="this.printStack" class="print-stack"> PRINT STACK </button>
-      <button @click="this.printInverseStack" class="print-stack"> PRINT inverse STACK </button>
-    </div>
-    <div class="back-forth-container">
-      <button v-if="this.stackIndex > 0" @click="this.previousCommand" class="back-button"> PREVIOUS </button>
-      <button v-if="this.stackIndex < this.commandStack.length - 1" @click="this.nextCommand" class="back-button"> NEXT </button>
-    </div> -->
   </div>
 </template>
 
 <script>
-import { ipcRenderer } from 'electron'
-const ipc = require("electron").ipcRenderer
-import { getStatus } from '../../utils/getStatus'
-import Viz from './Visualization.vue'
-import classification, { ACTIONS } from './GitCommandClassification'
-import inverseCommand from './GitInverseCommands'
-
-const channel = 'terminal.toTerm';
+// import { ipcRenderer } from 'electron'
+// const ipc = require("electron").ipcRenderer
+import StatusViz from './StatusViz.vue'
 
 export default {
   name: 'VizWindow',
   data() {
     return {
-      command: '',
-      currCommand: '',
-
       stackIndex: 0,
       commandStack: [{
         current: {
@@ -54,7 +35,7 @@ export default {
     }  
   },
   components: {
-    Viz,
+    StatusViz,
   },
   mounted() {
     const userInputChannel = 'user_input';
