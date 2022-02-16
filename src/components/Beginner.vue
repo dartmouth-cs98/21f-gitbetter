08d6c20477e262fd66ef1109ed76c2087f255588
Hailey Fox <hailey.p.fox.22@dartmouth.edu>
2022-02-16 00:24:28 -0500

    Continuing to work on Beginner code

---
+++ src/components/Beginner.vue
@@ -0,0 +1,243 @@
+
+<template>
+<div class="main">
+  <BeginnerNav  @help="resetStep" v-bind:ind="step">
+    <FirstTime 
+      @endTutorial="onEndTutorial" 
+      @updateStep="onUpdateStep" 
+      v-bind:ind="step" 
+      v-show="step >= 2"
+    />
+  </BeginnerNav>
+  <div class="columns">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <div class="top-wrapper column" id="top-wrapper">
+          <div id="terminal-wrapper" class="terminal-wrapper">
+            <div class="headline-component">
+              <div style="margin-left:auto;">
+                Tutorial Home
+              </div>
+              <div class="visButtons">
+                <button 
+                  id="opening-icon" 
+                  class="button"
+                  @click="openVisualization"
+                  title="Show visualization window"
+                >
+                  <font-awesome-icon icon="eye"/>
+                </button>
+              </div>
+            </div>
+            <div class="help-window">
+                <FirstTime 
+                  @updateStep="onUpdateStep" 
+                  @endTutorial="onEndTutorial" 
+                  v-bind:ind="step" 
+                  v-show="step == 0"
+                /> 
+            </div>
+              <TutorialHome />
+          </div>
+          <div id="visualizations" class="visualizations-wrapper">
+            <div class="headline-component">
+              <div style="margin-left:35%;">
+                Visualizations 
+              </div>
+              <div class="visButtons">
+                <button 
+                    id="closing-icon" 
+                    class="button"
+                    @click="closeVisualization"
+                    title="Hide visualization window"
+                  >
+                    <font-awesome-icon icon="eye-slash"/>
+                  </button>
+              </div>
+            </div>
+            <div class="help-window-two">
+                <FirstTime 
+                  @updateStep="onUpdateStep" 
+                  @endTutorial="onEndTutorial" 
+                  v-bind:ind="step" 
+                  v-show="step == 1"
+                /> 
+            </div>
+            <VizWindow />
+          </div>
+      </div>   
+  </div>
+</div>
+</template>
+
+<script>
+import BeginnerViz from './Visualizations/BeginnerViz.vue'
+import TutorialHome from './Practice/TutorialHome.vue'
+import BeginnerNav from '../router/BeginnerNav.vue'
+import FirstTime from './FirstTime.vue'
+
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+Vue.use(VueMaterial)
+
+export default {
+  name: 'Beginner',
+  components: {
+    BeginnerViz,
+    TutorialHome,
+    BeginnerNav,
+    FirstTime
+  },
+  data() {
+    return {
+      step: -1,
+    }
+  },
+  created() {
+    // this is where we'll get the result of git config user.name, 
+    // if it doesn't exist in the DB will set step to 0 and start tutorial
+    this.step = 0;
+  },
+  methods: {
+    closeVisualization() {
+      document.getElementById("visualizations").style.display = "none";
+      document.getElementById("opening-icon").style.display = "block";
+      document.getElementById("terminal-wrapper").style.width = "100%";
+    },
+    openVisualization() {
+      document.getElementById("visualizations").style.display = "block";
+      document.getElementById("terminal-wrapper").style.width = "50%";
+      document.getElementById("opening-icon").style.display = "none";
+    },
+    onUpdateStep(val) {
+      if(val == -1 && this.step == 0) {
+          this.step = 0;
+      }
+      else if(this.step == 5 && val == 1) {
+          this.step = -1;
+      }
+      else {
+          this.step = this.step + val;
+      }
+
+      console.log(this.step)
+    },
+    resetStep() {
+      this.step = 0;
+    },
+     onEndTutorial() {
+      this.step = -1;
+    },
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Material+Icons");
+.columns {
+    height: 100vh;
+}
+.column {
+  width: 83%;
+}
+.general-wrapper {
+  display:flex;
+}
+.top-wrapper {
+  display:flex;
+  padding-left: 0;
+  max-width: 80%;
+}
+.headline-component {
+  background-color: #53336B;
+  border-color: green;
+  color: white;
+  border-width: 1px;
+  border-style: solid;
+  font-weight:bold;
+  font-size: 20px;
+  padding: 15px 0px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  min-height: 72px;
+}
+#terminal-wrapper {
+  width: 50%;
+  background-color: black;
+  position: relative;
+  border-color: green;
+}
+.visualizations-wrapper {
+  width: 50%;
+  position: relative;
+}
+.closing-icon {
+  background-color: #ce93d8;
+  border-radius: 15px;
+  position: absolute;
+  right: 0;
+  top: 5px;
+}
+#opening-icon {
+  display:none;
+}
+#open-side-bar{
+  display:none;
+  background-color: #272727;
+  border-color: green;
+  border-width: 1px;
+  border-style: solid;
+  padding: 1% 0%;
+  width: 48px;
+  margin-top: 0.75rem;
+}
+
+.faIcon {
+    width: 3em;
+    cursor: pointer;
+    color:white;
+}
+
+
+.visButtons {
+  margin-right: 12px;
+  margin-left: auto;
+}
+
+.help-window { 
+    position: absolute;
+    right: 12px;
+    top: 3.75rem;
+    z-index: 6;
+    max-width: 60%;
+  }
+
+  .help-window-two { 
+    position: absolute;
+    left: 12px;
+    top: 3.25rem;
+    z-index: 6;
+    max-width: 50%;
+  }
+
+@media only screen and (max-width: 770px) {
+  .terminal-wrapper {
+    margin-left: 12px;
+  }
+  .top-wrapper {
+    height: 100%;
+  }
+  #opening-icon, #closing-icon {
+    width: 15%;
+    margin: 0;
+  }
+  .top-wrapper {
+    min-width: 100%;
+  }
+}
+</style>
---
+++ src/router/BeginnerNav.vue
@@ -0,0 +1,189 @@
+<template>
+    <nav class="nav level has-shadow" >
+      <div class="level-item has-text-centered">
+        <div class="nav level l-menu">
+          <router-link 
+              class="level-item has-text-centered"
+              :to="'/help'"
+              style="color:black; font-weight:500;"
+          >
+              Help
+          </router-link>
+
+          <router-link 
+              class="level-item has-text-centered"
+              :to="'/tutorials'"
+              style="color:black; font-weight:500;"
+          >
+              Tutorials
+          </router-link>
+        </div>
+      </div>
+
+
+        <div class="level-item has-text-centered">
+          <router-link 
+            class="level-item has-text-centered"
+            :to="'/beginner'"
+            style="color:black; font-weight:500;"
+          >
+          <img src="../assets/logo.png" />
+          </router-link>
+        </div>
+        <div class="level-item has-text-centered">
+          <div class="nav level r-menu">
+            <router-link
+                class="level-item has-text-centered"
+                style="color:black; font-weight:500;cursor:pointer;"
+                :to="'/doityourself'"
+                >
+                    Do It Yourself
+            </router-link>
+            <div
+                class="level-item has-text-centered"
+                style="color:black; font-weight:500;cursor:pointer;"
+                @click="$router.push('/')"
+                v-on:click="startOver"
+                >
+                    End
+            </div>
+
+
+            <div
+                class="level-item has-text-centered"
+                style="color:black; font-weight:500;cursor:pointer;"
+                v-on:click="$emit('help')"
+                >
+                    <font-awesome-icon icon="question-circle"/>
+            </div>
+          </div>
+        </div>
+      <loading />
+      <div v-bind:class="helpClass">
+        <slot></slot>
+      </div>
+    </nav>
+</template>
+
+<script>
+var start_over = require('../utils/start_over')
+import Loading from '../components/Loading.vue'
+
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          text: 'Welcome',
+          page:'/'
+        },
+        {
+          id: 1,
+          text: 'Beginner',
+          page:'/beginner'
+        },
+        {
+          id: 2,
+          text: 'Help',
+          page:'/help'
+        },
+        {
+          id: 3,
+          text: 'Tutorials',
+          page:'/tutorials'
+        },
+        {
+          id: 4,
+          text: 'DoItYourself',
+          page:'/doityourself'
+        },
+      ],
+      load: false,
+    }
+  },
+  props: {
+    ind: Number,
+  },
+  inject: ['isLoading'],
+  provide() {
+    return {
+      isLoading: this.load,
+    };
+  },
+  components: {
+    Loading,
+  },
+  computed: {
+    helpClass() {
+      return {
+        helpone: this.ind == 2,
+        helptwo: this.ind == 3,
+        helpthree: this.ind == 4,
+        helpfour: this.ind == 5,
+      }
+    }
+  },
+  methods: {
+    async startOver() {
+     this.isLoading.value = true;
+      await start_over.start_over()
+      this.isLoading.value = false;
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .nav {
+      margin-bottom: 0 !important;
+      height: 5rem;
+      background-color: #ab47bc;
+  }
+  img {
+    width: 45%;
+    padding: 10px;
+  }
+
+  .l-menu {
+    width: 100%;
+  }
+  .r-menu {
+    width: 100%;
+  }
+  .helpone { 
+    position: absolute;
+    left: 24px;
+    top: 4.6rem;
+    z-index: 6;
+    max-width: 25%;
+  }
+  .helptwo { 
+    position: absolute;
+    left: 15%;
+    top: 4.6rem;
+    z-index: 6;
+    max-width: 25%;
+  }
+  .helpthree { 
+    position: absolute;
+    left: 45%;
+    top: 4.6rem;
+    z-index: 6;
+    max-width: 25%;
+  }
+  .helpfour { 
+    position: absolute;
+    right: 12px;
+    top: 4.6rem;
+    z-index: 6;
+    max-width: 25%;
+  }
+
+  @media only screen and (max-width: 770px) {
+    .nav {
+      display: flex;
+      padding: 0px 30px;
+    }
+  }
+</style>
