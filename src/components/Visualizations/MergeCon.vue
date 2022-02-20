c669dad9e56441e31c36fb94777777b14a4956e1
Kimberley R <kimberley.rangel.campos.22@dartmouth.edu>
2022-02-20 17:05:13 -0500

    Added markdown - not sure if we'll keep it

--- src/components/Visualizations/MergeCon.vue
+++ src/components/Visualizations/MergeCon.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="top-layer">
         <div class="title-area">{{this.mergeSteps.title}}</div>
-        <div class="content"> {{this.mergeSteps.steps[index].step}}</div>
+        <div class="content"> {{this.mergeSteps.steps[this.index].step}}</div>
+        <div class="command">
+            <div class="mark-down">
+                {{ this.mergeSteps.steps[index].mark }}</div>
+            </div>
         <div id="button-area">
             <button v-if="this.index > 0" @click="backStep"  class="button">Back</button>
             <button v-if="this.index + 1< this.content.length" @click="nextStep"  class="button">Next</button>
@@ -11,6 +15,7 @@
 </template>
 <script>
 import { mergeInterface } from '../../constants/mergeConflict.js'
+import { marked } from 'marked';
 
 export default {
     name: 'MergeCon',
@@ -19,7 +24,11 @@ export default {
             mergeSteps: mergeInterface,
             content: mergeInterface.steps,
             index: 0,
+            markdown:  " `Hello World`",
             }
+    },
+    computed: {
+        
     },
     methods: {
         nextStep() {
@@ -28,6 +37,10 @@ export default {
         backStep() {
             this.index = this.index - 1
         },
+        markdownToHtml(text){
+            console.log(text);
+            return marked(text);
+        },
     }
 }
 
@@ -35,13 +48,29 @@ export default {
 <style scoped>
 .top-layer{
     height:100%;
+    display:flex;
+    flex-direction: column;
+    align-items: center;
 }
 .title-area{
     font-size: 2rem;
     color: white;
+    padding: 2%;
 }
 .content{
     height:80%;
+}
+.mark-down {
+    background-color: #636363;
+    color: #FFFFFF;
+    font-size: 18px;
+    padding-right: 12px;
+}
+
+.command {
+    display: flex;
+    justify-content: center;
+    padding-top: 10%;
 }
 
 .button-area{
