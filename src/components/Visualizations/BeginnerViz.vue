e48a89db15457ce4b96e77bc1da410335897c949
Hailey Fox <hailey.p.fox.22@dartmouth.edu>
2022-03-15 20:39:34 -0500

    Pull visualization

--- src/components/Visualizations/BeginnerViz.vue
+++ src/components/Visualizations/BeginnerViz.vue
@@ -24,12 +24,6 @@ export default {
   },
   methods: {
     imgPath() {
-        // if (this.command == "") {
-        //     return require ("../../assets/demo/openingrepo.png");
-        // } 
-        // if (this.command == "") {
-        //     return require("../../assets/demo/clonerepo.png")
-        // }
         if (this.command == "git clone https://github.com/dartmouth-cs98/GitBetterTestRepository.git") {
             this.source = require("../../assets/demo/begclone.png")
         } else if (this.command == "git checkout -b my_branch") {
@@ -42,38 +36,13 @@ export default {
             this.source = require("../../assets/demo/begcommitupdate.png")
         } else if (this.command == "git log") {
             this.source = require("../../assets/demo/beglog.png")
-        } else if (this.command == "git checkout main") {
-            this.souce = require("../../assets/demo/begcheckoutmain.png")
         } else if (this.command == "git push") {
             this.source = require("../../assets/demo/begpushupdate.png")
+        } else if (this.command == "git checkout main") {
+            this.souce = require("../../assets/demo/begcheckoutmainupdate.png")
+        } else if (this.command == "git pull") {
+            this.source = require("../../assets/demo/begpull.png")
         }
-        // } else if (this.command == "git branch") {
-        //     this.source = require("../../assets/demo/branch-dark.png");
-        // } else if (this.command == "git diff") {
-        //     this.source = require("../../assets/demo/diff-dark.png");
-        // } else if (this.command == "git status") {
-        //     this.source = require("../../assets/demo/status-dark.png");
-        // } else if (this.command == "git log") {
-        //     this.source = require("../../assets/demo/log-dark.png");
-        // } else if (this.command == "git fetch") {
-        //     this.source = require("../../assets/demo/fetch-dark.png");
-        // } else if (this.command == "git push") {
-        //     this.source = require("../../assets/demo/push-remote-dark.png");
-        // } else if (this.command == "git branch -u origin/master") {
-        //     this.source = require("../../assets/demo/origin-master-dark.png");
-        // } else if (this.command == "gitbetter visualize") {
-        //     this.source = require("../../assets/demo/visualize-remote-dark.png");
-        // } else if (this.command.startsWith("git checkout -b")) {
-        //     this.source = require("../../assets/demo/checkout-remote-dark.png");
-        // } else if (this.command.startsWith("git commit")) {
-        //     this.source = require("../../assets/demo/commit-remote-dark.png");
-        // } else if (this.command.startsWith("git pull")) {
-        //     this.source = require("../../assets/demo/pull-dark.png");
-        // } else if (this.command.startsWith("git revert")) {
-        //     this.source = require("../../assets/demo/revert-remote-dark.png");
-        // } else {
-        //     this.source = "";
-        // }
     }
   }
 }
